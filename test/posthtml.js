cd2497963afb51401c931ad2eb526b0a6f2b3a91
Tsuguya Toma <toma_tsuguya@cayounglab.co.jp>
2017-12-20 16:08:21 +0900

    posthtml test error handling.

--- test/posthtml.js
+++ test/posthtml.js
@@ -1,8 +1,12 @@
-const posthtml = require('posthtml');
+const parser = require('posthtml-parser');
+const render = require('posthtml-render');
+const api = require('posthtml/lib/api');
+
 const srcset = require('../lib/');
 
 module.exports = (html, opt) => {
-  return posthtml(srcset(opt))
-    .process(html)
-    .then(tree => tree.html);
+  const tree = parser(html);
+  tree.match = api.match;
+  srcset(opt)(tree);
+  return render(tree);
 };
