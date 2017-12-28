8e9b03ff3997a1d8c0bb67bb61719129a4861953
Tsuguya Toma <toma_tsuguya@cayounglab.co.jp>
2017-12-28 18:48:49 +0900

    use sync.

--- test/posthtml.js
+++ test/posthtml.js
@@ -1,12 +1,4 @@
-const parser = require('posthtml-parser');
-const render = require('posthtml-render');
-const api = require('posthtml/lib/api');
-
 const srcset = require('../lib/');
+const posthtml = require('posthtml');
 
-module.exports = (html, opt) => {
-  const tree = parser(html);
-  tree.match = api.match;
-  srcset(opt)(tree);
-  return render(tree);
-};
+module.exports = (html, opt) => posthtml().use(srcset(opt)).process(html, { sync: true }).html;
